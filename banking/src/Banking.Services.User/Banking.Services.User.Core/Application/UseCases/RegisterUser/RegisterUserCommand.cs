27c226170ba9f96911d5699540eb66285ebf30e1
Your Name <your.email@example.com>
2025-06-04 19:29:18 +0300

    Add RegisterUserCommand

--- banking/src/Banking.Services.User/Banking.Services.User.Core/Application/UseCases/RegisterUser/RegisterUserCommand.cs
+++ banking/src/Banking.Services.User/Banking.Services.User.Core/Application/UseCases/RegisterUser/RegisterUserCommand.cs
@@ -3,11 +3,10 @@ using MediatR;
 
 namespace Banking.Services.User.Core.Application.UseCases.RegisterUser;
 
-public class RegisterUserCommand : IRequest<UserResponse>
+public record RegisterUserCommand : IRequest<UserResponse>
 {
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string ConfirmPassword { get; set; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
 }
