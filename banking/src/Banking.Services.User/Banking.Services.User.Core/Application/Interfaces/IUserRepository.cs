802814d83e9c0e8c4fc298448987f9c5f322e8c7
Your Name <your.email@example.com>
2025-06-04 19:28:57 +0300

    Add IUserRepository interface

--- banking/src/Banking.Services.User/Banking.Services.User.Core/Application/Interfaces/IUserRepository.cs
+++ banking/src/Banking.Services.User/Banking.Services.User.Core/Application/Interfaces/IUserRepository.cs
@@ -1,17 +1,12 @@
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Banking.Services.User.Core.Domain.Entities;
 
-namespace Banking.Services.User.Core.Application.Interfaces
+namespace Banking.Services.User.Core.Application.Interfaces;
+
+public interface IUserRepository
 {
-    public interface IUserRepository
-    {
-        Task<Banking.Services.User.Core.Domain.Entities.User> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-        Task<Banking.Services.User.Core.Domain.Entities.User> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
-        Task<IEnumerable<Banking.Services.User.Core.Domain.Entities.User>> GetAllAsync(CancellationToken cancellationToken = default);
-        Task AddAsync(Banking.Services.User.Core.Domain.Entities.User user, CancellationToken cancellationToken = default);
-        Task UpdateAsync(Banking.Services.User.Core.Domain.Entities.User user, CancellationToken cancellationToken = default);
-        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
-    }
-} 
+    Task<User> GetByIdAsync(Guid id);
+    Task<User> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(Guid id);
+}
