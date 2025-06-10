9e060c7a66ce3f9b57ed2acda326fecfa1ad1b8d
teende <denis2007083@gmail.com>
2025-06-10 18:38:03 +0300

    BNK-3: implement secure password hashing with BCypt

--- banking/src/Banking.Services.User/Banking.Services.User.Infrastructure/Services/PasswordHasher.cs
+++ banking/src/Banking.Services.User/Banking.Services.User.Infrastructure/Services/PasswordHasher.cs
@@ -1,1 +1,18 @@
- 
+using Banking.Services.User.Core.Application.Interfaces;
+using BCrypt.Net;
+
+namespace Banking.Services.User.Infrastructure.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+        }
+
+        public bool VerifyPassword(string hash, string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+} 
