936bf437e29f4a3ad300b6fafb59ff2207f343fc
teende <denis2007083@gmail.com>
2025-07-07 00:20:02 +0300

    BNK-5: Add DTOs for money transfer operation in Account API

---
+++ banking/src/Banking.Services.Account.Api/Dtos/TransferMoneyDto.cs
@@ -0,0 +1,13 @@
+namespace Banking.Services.Account.Api.Dtos
+{
+    public class TransferMoneyDto
+    {
+        public Guid FromAccountId { get; set; }
+        public Guid ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+
+        public bool Success {get;set;}
+        public string ErrorMessage { get; set; }
+    }
+} 
