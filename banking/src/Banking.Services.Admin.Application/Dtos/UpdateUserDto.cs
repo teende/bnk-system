c5ada29d529cbb1bc51af39dd47136ce48c042d5
teende <denis2007083@gmail.com>
2025-07-09 01:16:42 +0300

    BNK-5: add UpdateUserDto for updating user details in admin module

---
+++ banking/src/Banking.Services.Admin.Application/Dtos/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+namespace Banking.Services.Admin.Application.Dtos
+{
+    public class UpdateUserDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        // TODO: Add more fields as needed
+    }
+} 
