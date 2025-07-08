f5d75f2c7bad03627ef32fe37e6c68c59694face
teende <denis2007083@gmail.com>
2025-07-09 01:30:45 +0300

    Удалены Card и Card.Core из ветки BNK-5

--- banking/src/Banking.Services.Account.Core/Application/IAccountService.cs
+++
@@ -1,9 +0,0 @@
-using Banking.Services.Account.Api.Dtos;
-
-namespace Banking.Services.Account.Core.Application
-{
-    public interface IAccountService
-    {
-        TransferResultDto TransferMoney(TransferMoneyDto dto);
-    }
-} 
