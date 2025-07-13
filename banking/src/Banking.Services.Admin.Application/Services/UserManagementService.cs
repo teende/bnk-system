1898c08186d71c7ca7654f8135f08031d0d6d96c
teende <denis2007083@gmail.com>
2025-07-14 01:57:13 +0300

    Remove UserManagementService.cs - implementation not ready

--- banking/src/Banking.Services.Admin.Application/Services/UserManagementService.cs
+++
@@ -1,9 +0,0 @@
-using Banking.Services.Admin.Application.Dtos;
-
-namespace Banking.Services.Admin.Application.Services
-{
-    public class UserManagementService : IUserManagementService
-    {
-        // TODO: Implement user management logic (CRUD, authentication, authorization, account requests)
-    }
-} 
