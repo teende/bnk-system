52a1af8217076d269f9d9a0bbb6da934771df33f
sq <85311776+teende@users.noreply.github.com>
2025-07-17 09:06:23 +0300

    Resolve conflicts and standardize TransferMoneyDto structure

--- banking/src/Banking.Services.Account.Api/Dtos/TransferMoneyDto.cs
+++ banking/src/Banking.Services.Account.Api/Dtos/TransferMoneyDto.cs
@@ -6,11 +6,7 @@ namespace Banking.Services.Account.Api.Dtos
         public Guid ToAccountId { get; set; }
         public decimal Amount { get; set; }
         public string Currency { get; set; }
-
-<<<<<<< HEAD
-=======
         public bool Success {get;set;}
         public string ErrorMessage { get; set; }
->>>>>>> feature/BNK-4-cards-and-accounts
     }
 } 
