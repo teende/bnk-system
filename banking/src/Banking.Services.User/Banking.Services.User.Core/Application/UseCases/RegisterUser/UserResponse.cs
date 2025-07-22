1ef3c583f608fef19f739c337ece17b42f94db7f
teende <denis2007083@gmail.com>
2025-07-22 17:28:53 +0300

    Add UserResponse DTO to encapsulate registration response data

---
+++ banking/src/Banking.Services.User/Banking.Services.User.Core/Application/UseCases/RegisterUser/UserResponse.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Banking.Services.User.Core.Application.UseCases.RegisterUser
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+} 
